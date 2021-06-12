84056663be791a0b8a832af14e8442553042f7b9
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:15:11 +0500

    Create longer_string method

--- bin/main.rb
+++ bin/main.rb
@@ -62,3 +62,16 @@ end
 
 puts larger_number(42, 28)   # => 42
 puts larger_number(99, 100)  # => 100
+# 6---
+# Write a method longer_string(str1, str2) that takes in two strings and returns the longer of the two strings. In the case of a tie, the method should return the first string.
+def longer_string(str1, str2)
+	if str1.length>=str2.length
+      return str1
+    else
+      str2
+    end
+end
+
+puts longer_string("app", "academy") # => "academy"
+puts longer_string("summer", "fall") # => "summer"
+puts longer_string("hello", "world") # => "hello"
