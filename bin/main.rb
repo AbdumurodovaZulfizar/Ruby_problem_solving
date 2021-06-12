5b855e4678c731432d28b06776bd5f232db32353
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:27:21 +0500

    Create first_in_array method

--- bin/main.rb
+++ bin/main.rb
@@ -232,3 +232,15 @@ end
 puts to_initials("Kelvin Bridges")      # => "KB"
 puts to_initials("Michaela Yamamoto")   # => "MY"
 puts to_initials("Mary La Grange")      # => "MLG"
+# 17---
+# Write a method first_in_array that takes in an array and two elements, the method should return the element that appears earlier in the array.
+def first_in_array(arr, el1, el2)
+	if arr.index(el1)>arr.index(el2)
+      return el2
+    else
+      el1
+    end
+end
+
+puts first_in_array(["a", "b", "c", "d"], "d", "b"); # => "b"
+puts first_in_array(["cat", "bird" ,"dog", "mouse" ], "dog", "mouse"); # => "dog"
