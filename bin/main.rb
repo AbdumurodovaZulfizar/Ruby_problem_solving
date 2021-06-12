42bdde379efadd4711f4075967647f00cacd53ac
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:14:15 +0500

    Create larger_number method

--- bin/main.rb
+++ bin/main.rb
@@ -50,3 +50,15 @@ puts either_only(20) # => true
 puts either_only(7)  # => false
 puts either_only(15) # => false
 puts either_only(30) # => false
+# 5---
+# Write a method larger_number(num1, num2) that takes in two numbers and returns the larger of the two numbers.
+def larger_number(num1, num2)
+	if num1>num2
+      return num1
+    else
+      num2
+    end
+end
+
+puts larger_number(42, 28)   # => 42
+puts larger_number(99, 100)  # => 100
