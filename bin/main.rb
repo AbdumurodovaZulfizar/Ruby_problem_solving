5e343fd3efddc4a85fe7533d8f0738a1d25dbfe1
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:21:16 +0500

    Create count_vovels method

--- bin/main.rb
+++ bin/main.rb
@@ -117,25 +117,43 @@ def count_e(word)
       i+=1
     end
     return n
-  end
+end
   
-  puts count_e("movie") # => 1
-  puts count_e("excellent") # => 3
-  # 10---
-  # Write a method count_a(word) that takes in a string word and returns the number of a's in the word. The method should count both lowercase (a) and uppercase (A)
-  def count_a(word)
-    i=0
-    n=0
-      while i<word.length
-        if word[i]=='a' || word[i]=='A'
-          n+=1
-        end
-        i+=1
+puts count_e("movie") # => 1
+puts count_e("excellent") # => 3
+# 10---
+# Write a method count_a(word) that takes in a string word and returns the number of a's in the word. The method should count both lowercase (a) and uppercase (A)
+def count_a(word)
+  i=0
+  n=0
+    while i<word.length
+      if word[i]=='a' || word[i]=='A'
+        n+=1
       end
-      return n
+      i+=1
     end
+  return n
+end
     
-    puts count_a("application")  # => 2
-    puts count_a("bike")         # => 0
-    puts count_a("Arthur")       # => 1
-    puts count_a("Aardvark")     # => 3
+puts count_a("application")  # => 2
+puts count_a("bike")         # => 0
+puts count_a("Arthur")       # => 1
+puts count_a("Aardvark")     # => 3
+# 11---
+# Write a method, count_vowels(word), that takes in a string word and returns the number of vowels in the word. Vowels are the letters a, e, i, o, u.
+def count_vowels(word)
+  i=0
+  n=0
+    vovels = 'aeiou'
+    while i<word.length
+      if vovels.include?(word[i])
+        n+=1
+      end
+      i+=1
+    end
+  return n
+end
+  
+puts count_vowels("bootcamp")  # => 3
+puts count_vowels("apple")     # => 2
+puts count_vowels("pizza")     # => 2
