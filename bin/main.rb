ccf6b087f4a0fb4f858780ee9ed8c3808100a100
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:50:29 +0500

    Create rotate_array method

--- bin/main.rb
+++ bin/main.rb
@@ -362,3 +362,18 @@ end
 
 puts reverse_words('keep coding') # => 'peek gnidoc'
 puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
+# 23---
+# Write a method rotate_array that takes in an array and a number. The method should return the array after rotating the elements the specified number of times. A single rotation takes the last element of the array and moves it to the front.
+def rotate_array(arr, num)
+num.times do
+  ele = arr.pop
+  arr.unshift(ele)
+end
+return arr
+end
+  
+print rotate_array([ "Matt", "Danny", "Mashu", "Matthias" ], 1) # => [ "Matthias", "Matt", "Danny", "Mashu" ]
+puts
+  
+print rotate_array([ "a", "b", "c", "d" ], 2) # => [ "c", "d", "a", "b" ]
+puts
