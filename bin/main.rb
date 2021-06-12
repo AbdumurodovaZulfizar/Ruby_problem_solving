7746be69cfab46eb7a59fac85021c937cd6d6ca2
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:30:36 +0500

    Create format_name method

--- bin/main.rb
+++ bin/main.rb
@@ -289,3 +289,31 @@ end
 
 puts format_name("chase WILSON") # => "Chase Wilson"
 puts format_name("brian CrAwFoRd scoTT") # => "Brian Crawford Scott"
+# 20---
+# Write a method is_valid_name that takes in a string and returns a boolean indicating whether or not it is a valid name.
+# A name is valid is if satisfies all of the following:
+# - contains at least a first name and last name, separated by spaces
+# - each part of the name should be capitalized
+#
+# Hint: use str.upcase or str.downcase
+# "a".upcase # => "A"
+
+def is_valid_name(str)
+	arr= str.split(' ')
+  	if arr.length<2
+      return false
+    elsif arr.length>=2
+      arr.each do |name|
+        if name == name[0].upcase+name[1..-1].downcase
+          return true
+        else
+          return false
+        end
+      end
+    end
+end
+
+puts is_valid_name("Kush Patel")       # => true
+puts is_valid_name("Daniel")           # => false
+puts is_valid_name("Robert Downey Jr") # => true
+puts is_valid_name("ROBERT DOWNEY JR") # => false
