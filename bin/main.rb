a535b6807d0ba83e63128186d61f9ed3ddc3884a
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:15:59 +0500

    Create number_check method

--- bin/main.rb
+++ bin/main.rb
@@ -75,3 +75,18 @@ end
 puts longer_string("app", "academy") # => "academy"
 puts longer_string("summer", "fall") # => "summer"
 puts longer_string("hello", "world") # => "hello"
+# 7---
+# Write a method number_check(num) that takes in a number and returns a string. The method should return the string 'positive' if the num is positive, 'negative' if the num is negative, and 'zero' if the num is zero.
+def number_check(num)
+	if num>0
+      return "positive"
+    elsif num<0
+      return "negative"
+    else
+      return 'zero'
+    end
+end
+
+puts number_check(5)    # => "positive"
+puts number_check(-2)   # => "negative"
+puts number_check(0)    # => "zero"
