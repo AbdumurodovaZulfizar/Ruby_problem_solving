b6f33947b8aceff8b7e6f3d371f4c8004f2aab5a
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:47:45 +0500

    Create is_valid_email method

--- bin/main.rb
+++ bin/main.rb
@@ -317,3 +317,34 @@ puts is_valid_name("Kush Patel")       # => true
 puts is_valid_name("Daniel")           # => false
 puts is_valid_name("Robert Downey Jr") # => true
 puts is_valid_name("ROBERT DOWNEY JR") # => false
+# 21---
+# Write a method is_valid_email that takes in a string and returns a boolean indicating whether or not it is a valid email address.
+# For simplicity, we'll consider an email valid when it satisfies all of the following:
+# - contains exactly one @ symbol
+# - contains only lowercase alphabetic letters before the @
+# - contains exactly one . after the @
+
+def is_valid_email(str)
+	letters = 'qwertyuiopasdfghjklzxcvbnm'
+  	arr = str.split('@')
+  	if arr.length != 2
+      return false
+    end
+  	arr[0].each_char do |ele|
+      if !letters.include?(ele)
+        return false
+      end
+    end
+  if arr[1].split('.').length==2
+    return true
+  else
+    return false
+  end
+end
+
+puts is_valid_email("abc@xy.z")         # => true
+puts is_valid_email("jdoe@gmail.com")   # => true
+puts is_valid_email("jdoe@g@mail.com")  # => false
+puts is_valid_email("jdoe42@gmail.com") # => false
+puts is_valid_email("jdoegmail.com")    # => false
+puts is_valid_email("az@email")         # => false
