2fc5d070143d665eca3ab0ea0b44c4821bd4b5bd
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:12:27 +0500

    Create is_div_five method

--- bin/main.rb
+++ bin/main.rb
@@ -18,3 +18,19 @@ end
 puts goodbye("Daniel")   # => "Bye Daniel."
 puts goodbye("Mark")     # => "Bye Mark."
 puts goodbye("Beyonce")  # => "Bye Beyonce."
+
+# 3---
+# Write a method is_div_by_5(number) that takes in a number and returns the boolean true if the given number is divisible by 5, false otherwise
+def is_div_by_5(number)
+  if number%5==0
+    return true
+  else
+    return false
+  end
+
+end
+
+puts is_div_by_5(10) # => true
+puts is_div_by_5(40) # => true
+puts is_div_by_5(42) # => false
+puts is_div_by_5(8)  # => false
