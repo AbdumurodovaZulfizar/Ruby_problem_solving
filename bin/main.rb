bc70598a8618a510406f1947e9fb6b0cd1cf5898
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:18:36 +0500

    Create count_a method

--- bin/main.rb
+++ bin/main.rb
@@ -121,3 +121,21 @@ def count_e(word)
   
   puts count_e("movie") # => 1
   puts count_e("excellent") # => 3
+  # 10---
+  # Write a method count_a(word) that takes in a string word and returns the number of a's in the word. The method should count both lowercase (a) and uppercase (A)
+  def count_a(word)
+    i=0
+    n=0
+      while i<word.length
+        if word[i]=='a' || word[i]=='A'
+          n+=1
+        end
+        i+=1
+      end
+      return n
+    end
+    
+    puts count_a("application")  # => 2
+    puts count_a("bike")         # => 0
+    puts count_a("Arthur")       # => 1
+    puts count_a("Aardvark")     # => 3
