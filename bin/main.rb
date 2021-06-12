15ad2c00e80868aa1d6a673dd8c0bc090b67da03
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:23:31 +0500

    Create factorial method

--- bin/main.rb
+++ bin/main.rb
@@ -172,3 +172,17 @@ end
 
 puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
 puts sum_nums(5) # => 15
+# 13---
+# Write a method factorial(num) that takes in a number num and returns the product of all numbers from 1 up to and including num.
+def factorial(num)
+	i=1
+	n=1
+  while i<=num
+    n*=i
+    i+=1
+  end
+  return n
+end
+
+puts factorial(3) # => 6, because 1 * 2 * 3 = 6
+puts factorial(5) # => 120, because 1 * 2 * 3 * 4 * 5 = 120
