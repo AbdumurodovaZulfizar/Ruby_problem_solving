a66c15b5ddc0454dd99dbded3714b2fb174b3227
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:16:53 +0500

    Create word-check method

--- bin/main.rb
+++ bin/main.rb
@@ -90,3 +90,18 @@ end
 puts number_check(5)    # => "positive"
 puts number_check(-2)   # => "negative"
 puts number_check(0)    # => "zero"
+# 8---
+# Write a method word_check(word) that takes in a word and returns a string. The method should return the string "long" if the word is longer than 6 characters, "short" if it is less than 6 characters, and "medium" if it is exactly 6 characters long.
+def word_check(word)
+	if word.length>6
+      return 'long'
+    elsif word.length<6
+      return 'short'
+    else
+      'medium'
+    end
+end
+
+puts word_check("contraption") # => "long"
+puts word_check("fruit")       # => "short"
+puts word_check("puzzle")      # => "medium"
