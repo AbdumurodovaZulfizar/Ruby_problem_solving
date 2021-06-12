2aecbe8fd5c347672b2f57c42d5026be4f5493fc
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:13:17 +0500

    Create either_only method

--- bin/main.rb
+++ bin/main.rb
@@ -34,3 +34,19 @@ puts is_div_by_5(10) # => true
 puts is_div_by_5(40) # => true
 puts is_div_by_5(42) # => false
 puts is_div_by_5(8)  # => false
+
+# 4---
+# Write a method either_only(number) that takes in a number and returns true if the number is divisible by either 3 or 5, but not both. The method should return false otherwise.
+def either_only(number)
+	if ((number % 5==0)||(number % 3==0))&&(!(number % 15==0))
+      return true
+    else
+      return false
+    end
+end
+
+puts either_only(9)  # => true
+puts either_only(20) # => true
+puts either_only(7)  # => false
+puts either_only(15) # => false
+puts either_only(30) # => false
