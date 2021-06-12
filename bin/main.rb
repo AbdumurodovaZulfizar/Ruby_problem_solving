3fc20f37e3c17d113a97e0ed0fdc3b9e1a65088d
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:17:47 +0500

    Create count_e method

--- bin/main.rb
+++ bin/main.rb
@@ -105,3 +105,19 @@ end
 puts word_check("contraption") # => "long"
 puts word_check("fruit")       # => "short"
 puts word_check("puzzle")      # => "medium"
+# 9---
+# Write a method count_e(word) that takes in a string word and returns the number of e's in the word
+def count_e(word)
+  i=0
+  n=0
+    while i<word.length
+      if word[i]=='e'
+        n+=1
+      end
+      i+=1
+    end
+    return n
+  end
+  
+  puts count_e("movie") # => 1
+  puts count_e("excellent") # => 3
