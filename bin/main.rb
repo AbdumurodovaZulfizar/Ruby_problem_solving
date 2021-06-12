363c00bb4d57d359e400ad35111a947421bf1ead
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-12 10:48:57 +0500

    Create reverse_words method

--- bin/main.rb
+++ bin/main.rb
@@ -348,3 +348,17 @@ puts is_valid_email("jdoe@g@mail.com")  # => false
 puts is_valid_email("jdoe42@gmail.com") # => false
 puts is_valid_email("jdoegmail.com")    # => false
 puts is_valid_email("az@email")         # => false
+# 22---
+# Write a method reverse_words that takes in a sentence string and returns the sentence with the order of the characters in each word reversed. Note that we need to reverse the order of characters in the words, do not reverse the order of words in the sentence.
+def reverse_words(sent)
+	arr=sent.split(' ')
+  new_arr=[]
+  	arr.each do |ele|
+      ele = ele.reverse
+      new_arr<<ele
+    end
+  return new_arr.join(' ')
+end
+
+puts reverse_words('keep coding') # => 'peek gnidoc'
+puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
